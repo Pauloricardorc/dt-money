edb1cbe01ad27b962d801eb98eec21491d72c0c5
pauloricardorc <pauloricardor0@gmail.com>
2022-08-22 18:58:44 -0400

    Toggle User And Admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import { DefaultTheme, ThemeProvider } from 'styled-components'
 import { buildAbility } from './contexts/authorization/ability'
 import { GuardContext } from './contexts/authorization/authorizationContext'
-import { getAbilityByUser } from './contexts/authorization/userAbilities'
+import {
+  getAbilityByUser,
+  UserType,
+} from './contexts/authorization/userAbilities'
 import { TransactionsProvider } from './contexts/TransactionsContext'
 import { Transactions } from './pages/Transactions'
 import { GlobalStyle } from './styles/global'
@@ -15,7 +19,8 @@ export function App() {
     'theme',
     themeDefault,
   )
-  const useAbility = getAbilityByUser('admin')
+  const [user, setUser] = useState<UserType>('user')
+  const useAbility = getAbilityByUser(user)
   const ability = buildAbility(useAbility)
 
   const toggleTheme = () => {
@@ -27,6 +32,9 @@ export function App() {
       <GuardContext.Provider value={ability}>
         <GlobalStyle />
 
+        <button onClick={() => setUser(user === 'user' ? 'admin' : 'user')}>
+          {user}
+        </button>
         <TransactionsProvider>
           <Transactions toggleTheme={toggleTheme} />
         </TransactionsProvider>
